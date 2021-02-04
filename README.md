ec6ee1f6bb3ad188d665d5383f7c1a52a7b6efa6
Abdullah Al Fahim <50063518+fahimw@users.noreply.github.com>
2021-02-04 23:41:22 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# weather-and-current-location
---
+++ script.js
@@ -0,0 +1,69 @@
+const search = document.getElementById('search');
+
+
+location_hadle();
+
+
+search.addEventListener('click', function() {
+    weather();
+})
+
+
+function weather() {
+    const inputValue = document.getElementById('customInput')
+    handle_fetch(inputValue.value);
+    
+}
+
+
+// api .this is about gps 
+function location_hadle(){
+   fetch('https://freegeoip.app/json/')
+    .then(response=> response.json())
+    .then(data=>{
+
+     const yourLocation = data.region_name
+     const countryName = data.country_name
+     const timeZone= data.time_zone
+       document.getElementById('location').innerText = yourLocation;
+       document.getElementById('timeZone').innerText = timeZone;
+       document.getElementById('countryName').innerText = countryName;
+
+
+        if(yourLocation !== ""){
+        document.getElementById('customInput').placeholder =yourLocation;
+        handle_fetch(yourLocation);
+        }else{
+               document.getElementById('customInput').placeholder =countryName;
+                handle_fetch(countryName);
+        }
+
+       console.log(data);
+        
+    })
+ 
+}
+    
+
+// Api handle from here
+
+
+function handle_fetch(presentLocation){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+presentLocation+'&units=metric&appid=61d2ad6f0b7758a658fa5e7db897fd42 ')
+        .then(response => response.json())
+        .then(data=>{
+            const cityName = data.name
+            const cityTemp = data.main
+            const temp = cityTemp.temp
+            const weather = data.weather[0]
+            const description = weather.description
+            let icon = weather.icon
+            const link ="http://openweathermap.org/img/w/"+icon+".png"
+            
+            document.getElementById("myImg").src = link
+            document.getElementById('cityName').innerText = cityName;
+            document.getElementById('temp').innerText = temp;
+            document.getElementById('description').innerText = description;
+
+        })
+}
